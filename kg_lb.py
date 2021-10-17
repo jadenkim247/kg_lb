1090d4e38baf3210e56c3e653cef82722a01efdf
jadenkim247 <89100127+jadenkim247@users.noreply.github.com>
2021-10-17 11:23:09 -0400

    Add files via upload

---
+++ kg_lb.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+def cal_kg():
+
+    kg = float(input("kg? >>>"))
+    lb = float(2.20462 * kg)
+
+    print(lb, "lb")
+
+
+# In[35]:
+
+
+def cal_lb():
+
+    lb = float(input("lb? >>>"))
+    kg = float(0.453592 * lb)
+
+    print(kg, "kg")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
